c6dc3c4068ac49a23d8ef44e2be1334440ccaa56
Roland Illig <roland.illig@gmx.de>
2022-02-25 23:00:33 +0100

    Clean up code for rating the app

--- app/src/main/java/atorch/statspuzzles/AppRater.java
+++ app/src/main/java/atorch/statspuzzles/AppRater.java
@@ -1,7 +1,6 @@
 package atorch.statspuzzles;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -10,7 +9,7 @@ import androidx.appcompat.app.AlertDialog;
 
 public class AppRater {
 
-    private final static double HOURS_BETWEEN_PROMPT = 25;  // Avoid spamming people with review prompts
+    private final static long HOURS_BETWEEN_PROMPT = 25;  // Avoid spamming people with review prompts
     private final static int LAUNCHES_UNTIL_PROMPT = 3;  // Let people open the app a few times before asking
     private final static int PUZZLES_SOLVED_UNTIL_PROMPT = 5;  // Want to prompt engaged users
 
@@ -23,14 +22,14 @@ public class AppRater {
         long launch_count = prefs.getLong("launch_count", 0) + 1;
         editor.putLong("launch_count", launch_count);
 
-        Long datePreviousPrompt = prefs.getLong("date_previous_prompt", 0);
+        long datePreviousPrompt = prefs.getLong("date_previous_prompt", 0);
         if (datePreviousPrompt == 0) {
             datePreviousPrompt = System.currentTimeMillis();
             editor.putLong("date_previous_prompt", datePreviousPrompt);
         }
 
         if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
-            double minimumCurrentTime = datePreviousPrompt + HOURS_BETWEEN_PROMPT * 60 * 60 * 1000;
+            long minimumCurrentTime = datePreviousPrompt + HOURS_BETWEEN_PROMPT * 60 * 60 * 1000;
             SharedPreferences appData = mContext.getSharedPreferences("atorch.statspuzzles.data", Context.MODE_PRIVATE);
             int solved0 = appData.getInt("solved_0", 0);
             int solved1 = appData.getInt("solved_1", 0);
@@ -52,30 +51,22 @@ public class AppRater {
         builder.setTitle(R.string.apprater_title);
         builder.setMessage(R.string.apprater_message);
         builder.setPositiveButton(R.string.apprater_rate_button,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        mContext.startActivity(new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("market://details?id=atorch.statspuzzles")));
-                        dialog.dismiss();
-                    }
+                (dialog, id) -> {
+                    mContext.startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=atorch.statspuzzles")));
+                    dialog.dismiss();
                 });
 
         builder.setNeutralButton(R.string.apprater_later_button,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.dismiss();
-                    }
-                });
+                (dialog, id) -> dialog.dismiss());
 
         builder.setNegativeButton(R.string.apprater_never_button,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        if (editor != null) {
-                            editor.putBoolean("dontshowagain", true);
-                            editor.commit();
-                        }
-                        dialog.dismiss();
+                (dialog, id) -> {
+                    if (editor != null) {
+                        editor.putBoolean("dontshowagain", true);
+                        editor.commit();
                     }
+                    dialog.dismiss();
                 });
         AlertDialog alert = builder.create();
         alert.show();
