bfed8661821a9194a82ff70ca379a2ae286a4ac9
Roland Illig <roland.illig@gmx.de>
2022-06-09 08:48:23 +0200

    Rename method parameters to not start with 'm'
    By convention, the leading 'm' is used in the Android code to mark
    fields of a class. Since parameters are more like local variables,
    remove the 'm'.
    
    No functional change.

--- app/src/main/java/atorch/statspuzzles/AppRater.java
+++ app/src/main/java/atorch/statspuzzles/AppRater.java
@@ -13,8 +13,8 @@ public class AppRater {
     private static final int LAUNCHES_UNTIL_PROMPT = 3;  // Let people open the app a few times before asking
     private static final int PUZZLES_SOLVED_UNTIL_PROMPT = 5;  // Want to prompt engaged users
 
-    public static void app_launched(Context mContext) {
-        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
+    public static void app_launched(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("apprater", Context.MODE_PRIVATE);
         if (prefs.getBoolean("dontshowagain", false)) {
             return;
         }
@@ -32,7 +32,7 @@ public class AppRater {
 
         if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
             long minimumCurrentTime = datePreviousPrompt + HOURS_BETWEEN_PROMPT * 60 * 60 * 1000;
-            SharedPreferences appData = mContext.getSharedPreferences("atorch.statspuzzles.data", Context.MODE_PRIVATE);
+            SharedPreferences appData = context.getSharedPreferences("atorch.statspuzzles.data", Context.MODE_PRIVATE);
             int solved0 = appData.getInt("solved_0", 0);
             int solved1 = appData.getInt("solved_1", 0);
             int solved2 = appData.getInt("solved_2", 0);
@@ -41,20 +41,20 @@ public class AppRater {
                     solvedTotal >= PUZZLES_SOLVED_UNTIL_PROMPT) {
                 datePreviousPrompt = System.currentTimeMillis();
                 editor.putLong("date_previous_prompt", datePreviousPrompt);
-                showRateDialog(mContext, editor);
+                showRateDialog(context, editor);
             }
         }
 
         editor.commit();
     }
 
-    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+    public static void showRateDialog(final Context context, final SharedPreferences.Editor editor) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.apprater_title);
         builder.setMessage(R.string.apprater_message);
         builder.setPositiveButton(R.string.apprater_rate_button,
                 (dialog, id) -> {
-                    mContext.startActivity(new Intent(Intent.ACTION_VIEW,
+                    context.startActivity(new Intent(Intent.ACTION_VIEW,
                             Uri.parse("market://details?id=atorch.statspuzzles")));
                     dialog.dismiss();
                 });
