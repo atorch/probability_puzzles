e0aaf10d7218d37627f5235b2c2fda1cadf3d40b
Adrian Torchiana <adrian.torchiana@gmail.com>
2023-01-17 15:54:53 -0800

    Deliberate code errors (make sure action fails with clear error message)

--- app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
+++ app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
@@ -20,6 +20,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+imprt this should fail with an obvious error!
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
