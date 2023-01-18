ea359ea878e4a3050a476354ef6644337563d8c2
Adrian Torchiana <adrian.torchiana@gmail.com>
2023-01-17 21:41:47 -0800

    Remove imprt line

--- app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
+++ app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
@@ -20,8 +20,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-imprt this should fail with an obvious error!
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
