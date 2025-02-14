3cf5bbd963283bd457a61e163dd0a7e660128c55
Adrian Torchiana <adrian.torchiana@gmail.com>
2025-02-13 22:35:11 -0800

    Gradle build should pass

--- app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
+++ app/src/main/java/atorch/statspuzzles/SolvePuzzle.java
@@ -1,6 +1,6 @@
 package atorch.statspuzzles;
 
-import android.app.AlertDialogs;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
